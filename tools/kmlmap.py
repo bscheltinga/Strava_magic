20f28a78f999b042df27b8da6afb1d0418b828d2
Daan <daangeijs@gmail.com>
2018-12-05 03:36:15 +0100

    refactoring and comments

--- tools/kmlmap.py
+++ tools/kmlmap.py
@@ -5,11 +5,9 @@ import time
 from tqdm import tqdm
 def create_kml(usertoken, df):
     client = Client(access_token=usertoken)
-
     # Initiate KML
     kml = simplekml.Kml()
-
-
+    # Get gps data from every activity
     for idx, row in tqdm(df.iterrows(), total=df.shape[0],desc='Creating KML file' ):
         if idx % 500 == 0 and idx > 1:  # To prevent exceeding strava limits
             print('Waiting for STRAVA API limits.')
@@ -17,8 +15,8 @@ def create_kml(usertoken, df):
         id = row['id']
         types = ['latlng']
         streams = client.get_activity_streams(id, types=types, resolution='medium')
-        if [True for x in streams.keys() if x == 'latlng']: #check if latlng data is available as datastream
+        if [True for x in streams.keys() if x == 'latlng']: # Check if latlng data is available as datastream
             cords = np.array(streams['latlng'].data)
             cords = np.flip(cords, axis=1)  # Flip the cords for right kml notation
-            ls = kml.newlinestring(name=row['name'], coords=cords, extrude=1)
+            kml.newlinestring(name=row['name'], coords=cords, extrude=1)
     kml.save(r"data/KMLmap.kml")
