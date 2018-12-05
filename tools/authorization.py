b3542e48243db60ac8990e0e157e57dfab5c7cfb
Daan <daangeijs@gmail.com>
2018-12-05 01:00:04 +0100

    refactor file

--- tools/authorization.py
+++ tools/authorization.py
@@ -4,17 +4,9 @@ import urllib.parse as urlparse
 import webbrowser
 import sys
 
-#Define the web functions to call from the strava API
-def UseCode(code):
-  client = stravalib.client.Client()
-  #Retrieve the login code from the Strava server
-  access_token = client.exchange_code_for_token(client_id=client_id,
-                                                client_secret=secret, code=code)
-  return access_token
-
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
   #Handle the web data sent from the strava API
-
+  access_token = []
   def do_HEAD(self):
     return self.do_GET()
 
@@ -23,7 +15,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     # self.wfile.write(b'<script>window.close();</script>')
     code = urlparse.parse_qs(urlparse.urlparse(self.path).query)['code'][0]
     # Login to the API
-    token = UseCode(code)
+    token = useCode(code)
     # Create page
     self.send_response(200)
     self.send_header(b"Content-type", "text/html")
@@ -34,14 +26,21 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     return self.save_token(token['access_token'])
 
   def save_token(self, token):
+      access_token = token
       with open(r'tokens\user_access.token', 'w') as file:
           file.write(token)
+def useCode(code):
+  # Put your data in file 'tokens/client.token' and separate the fields with a comma: clientid,clientsecrettoken
+  with open(r'tokens\client.token', 'r') as file:
+    client_secret = file.read().split(',')
+  client_id, secret = client_secret[0], client_secret[1]
+  client = stravalib.client.Client()
+  #Retrieve the login code from the Strava server
+  access_token = client.exchange_code_for_token(client_id=client_id,
+                                                client_secret=secret, code=code)
+  return access_token
 
-if __name__=='__main__':
-    #Put your data in file 'tokens/client.token' and separate the fields with a comma: clientid,clientsecrettoken
-    with open(r'tokens\client.token', 'r') as file:
-        client_secret = file.read().split(',')
-    client_id, secret = client_secret[0], client_secret[1]
+def authorize():
     port = 8008
     url = 'http://localhost:%d/authorization' % port
 
@@ -53,13 +52,13 @@ if __name__=='__main__':
                                    scope='activity:read_all')
     print('Opening: %s' % authorize_url)
     webbrowser.open(authorize_url)
+    a = MyHandler
     try:
-        httpd = BaseHTTPServer.HTTPServer(('localhost', port), MyHandler)
+        httpd = BaseHTTPServer.HTTPServer(('localhost', port), a)
         httpd.handle_request()
     except KeyboardInterrupt:
                     # Allow ^C to interrupt from any thread.
                     sys.stdout.write('\033[0m')
                     sys.stdout.write('User Interupt\n')
-
 
 
