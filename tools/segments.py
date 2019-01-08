64b84e7b0a910396dfd66be53bbbe910245ecd13
vuxxa <boukescheltinga@gmail.com>
2019-01-08 12:34:43 +0100

    fix idx counter

--- tools/segments.py
+++ tools/segments.py
@@ -8,8 +8,8 @@ from stravalib import Client
 def segmentlist(user_token, df):
     client = Client(access_token=user_token)
     df_segments = pd.DataFrame()
-
-    for idx, row in tqdm(df.iterrows(), total=df.shape[0],desc='Creating all ridden segments list'):
+    idx = 0
+    for a, row in tqdm(df.iterrows(), total=df.shape[0],desc='Creating all ridden segments list'):
         if idx % 500 == 0 and idx > 1:  # To prevent exceeding strava limits
             print('Waiting for STRAVA API limits.')
             time.sleep(900)
@@ -21,16 +21,12 @@ def segmentlist(user_token, df):
             entry = {'id' : last_act.segment_efforts[i].segment.id,
                      'name' : last_act.segment_efforts[i].name}
             df_segments = df_segments.append(entry, ignore_index=True)
-        print(idx)
 
-        if idx > 999:
-
+        if idx > 3:
             df_segments.drop_duplicates()
-            writer = pd.ExcelWriter('Segments.xlsx')
-            df_segments.to_excel(writer)
-            writer.save()
+            df_segments.to_excel(r'data\Segments.xlsx')
             break
-
+        idx += 1
     return df_segments
     # To get the segment standings:
     #segment = client.get_segment_leaderboard(segment_id=segment_id, gender='M') # Gender 'M'???
