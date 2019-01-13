e8f0cc5878c98964705efbf164b39054c98daece
vuxxa <boukescheltinga@gmail.com>
2019-01-13 23:20:08 +0100

    Removed bugs. To fix: STRAVA Limits

--- tools/segments.py
+++ tools/segments.py
@@ -8,23 +8,42 @@ from stravalib import Client
 def segmentlist(user_token, df):
     client = Client(access_token=user_token)
     df_segments = pd.DataFrame()
+    df_segments['id'] = np.nan # make the id column
     idx = 0
     for a, row in tqdm(df.iterrows(), total=df.shape[0],desc='Creating all segments list'):
         if idx % 550 == 0 and idx > 1:  # To prevent exceeding strava limits
             print('Waiting for STRAVA API limits.')
             time.sleep(900)
         id = row['id']
+        last_act = client.get_activity(activity_id=id, include_all_efforts=True) # Get activity
 
-        # Get from each activity the ridden/ran segments
-        last_act = client.get_activity(activity_id= id, include_all_efforts=True)
         for i in range(len(last_act.segment_efforts)):
-            entry = {'id' : last_act.segment_efforts[i].segment.id,
-                     'name' : last_act.segment_efforts[i].name}
-            df_segments = df_segments.append(entry, ignore_index=True)
+            segment_id = last_act.segment_efforts[i].segment.id
+            if sum(df_segments['id'].isin([segment_id])) == 0:  # Only for unique segments
+                entry = {'id': last_act.segment_efforts[i].segment.id,
+                         'name': last_act.segment_efforts[i].name,
+                         'distance': float(last_act.segment_efforts[i].distance.num),
+                         'activity_type': str(last_act.type),
+                         'average_grade': last_act.segment_efforts[i].segment.average_grade,
+                         'efforts': int(last_act.segment_efforts[i].segment.leaderboard.entry_count),
+                        }
+
+                for j in reversed(range(len(last_act.segment_efforts[i].segment.leaderboard.entries))):
+                    if str(last_act.segment_efforts[i].segment.leaderboard.entries[j].athlete_name) == 'Bouke S.':
+                        entry['rank'] = last_act.segment_efforts[i].segment.leaderboard.entries[j].rank
+                        entry['elapsed_time'] = last_act.segment_efforts[i].segment.leaderboard.entries[j].elapsed_time
+                        entry['pr_date'] = last_act.segment_efforts[i].segment.leaderboard.entries[j].start_date_local
+                        break
+
+                df_segments = df_segments.append(entry, ignore_index=True)
+
+            if idx == 1:
+                test = 100
+
         idx += 1
 
-    df_segments = df_segments.drop_duplicates()
-    df_segments.to_excel(r'data\Segments.xlsx')
+    df_segments = df_segments.sort_values(by=['kom_rank'])
+    df_segments.to_excel(r'data\Segmentsv2.xlsx')
     return df_segments
 
 # Later, make one function which combines segmentlist and segmentrating
@@ -35,7 +54,7 @@ def segmentrating(user_token, df_segments):
         segment = client.get_segment_leaderboard(segment_id, gender=None)
 
         if len(segment.entries) == 15:
-        entry = {'type': str(segment.kom_type),
+            entry = {'type': str(segment.kom_type),
                  'effort_count': int(segment.effort_count),
                  'rank': int(segment.entries[12].rank)} # Check if always at 12
 
