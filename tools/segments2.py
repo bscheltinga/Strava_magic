a612100afd5f852aa7e09765c3661dcfd97f89b9
vuxxa <boukescheltinga@gmail.com>
2020-04-13 12:32:04 +0200

    Flow of full sync is working.

--- tools/segments2.py
+++ tools/segments2.py
@@ -26,12 +26,31 @@ class SegmentsHandler(object):
         datarow = {'id': int(activity.id)}
         return datarow
 
+    def __handleSegment(self, segmenteffort):
+        datarow = {'id': int(segmenteffort.segment.id),
+                 'name': str(segmenteffort.name),
+                 'distance': float(segmenteffort.distance.num),
+                 'activity_type': str(segmenteffort.segment.activity_type),
+                 'average_grade': float(segmenteffort.segment.average_grade),
+                 'efforts': int(segmenteffort.segment.leaderboard.effort_count),
+                 'KOM_time': segmenteffort.segment.leaderboard.entries[1].elapsed_time
+                 }
+        return datarow
+
     def __getActivities(self):
         df = pd.DataFrame()
         activities = self.__api.get_activities()
         for i, activity in enumerate(activities):
             entry = self.__handleActivity(activity)
             df = df.append(entry, ignore_index=True)
+        return df
+
+    def __getSegments(self, last_act):
+        df = pd.DataFrame()
+        for i in range(len(last_act.segment_efforts)):
+            if last_act.segment_efforts[i].segment.hazardous == 0: # Only for segments with a leaderboard
+                entry = self.__handleSegment(last_act.segment_efforts[i])
+                df = df.append(entry, ignore_index=True)
         return df
 
     def __setdatatypes(self, df):
@@ -68,9 +87,14 @@ class SegmentsHandler(object):
         self.__test = 10 # DEbug option
 
     def full_sync(self):
-        print('**FULL SEGMENT LIST SYNC**')
+        df = pd.DataFrame()
+        print('**FULL SYNC SEGMENT LIST**')
         activities = self.__getActivities()
-        test =10
+        for i in range(len(activities)):
+            last_act = self.__api.get_activity(activity_id=activities.id[i], include_all_efforts=True)
+            entry = self.__getSegments(last_act)
+            df = df.append(entry, ignore_index=True)
+
         df = self.__replacegearid(df)
         self.__savefile(df)
 
