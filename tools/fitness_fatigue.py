fb1f2fbf9b8579e39a31a058fec7a31a63accb96
vuxxa <boukescheltinga@gmail.com>
2021-01-30 12:08:25 +0100

    Plot b_trimp over time

--- tools/fitness_fatigue.py
+++ tools/fitness_fatigue.py
@@ -6,3 +6,9 @@
 def b_trimp(df):
     df = df.loc[df['has_heartrate'] == True]
     b_trimps = (df['moving_time'] / np.timedelta64(1, 'h')) * df['average_heartrate']
+    return b_trimps
+
+# Plot trimp over time
+plt.plot(b_trimps.index[-100:-1], b_trimps[-100:-1])
+plt.xticks(rotation=45)
+plt.subplots_adjust(bottom=0.2)
