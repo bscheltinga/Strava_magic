8e127dde88cb5ce187aab261048caa153b385a2a
vuxxa <boukescheltinga@gmail.com>
2021-01-30 11:50:23 +0100

    Calculate banister's TRIMP from datafields

--- tools/fitness_fatigue.py
+++ tools/fitness_fatigue.py
@@ -1,4 +1,8 @@
 # Implementation of fitness fatigue model
+# assuming act. has HR_data
 # b_trimp: implementation of Banister' trimp (avg_hr*duration)
 
-def b_trimp
+
+def b_trimp(df):
+    df = df.loc[df['has_heartrate'] == True]
+    b_trimps = (df['moving_time'] / np.timedelta64(1, 'h')) * df['average_heartrate']
