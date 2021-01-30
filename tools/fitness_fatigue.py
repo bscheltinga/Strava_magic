764fe404323886dcd0ea78d6c736045bfeea702f
vuxxa <boukescheltinga@gmail.com>
2021-01-30 16:25:49 +0100

    Implemented fitness-fatigue model with plotting.

--- tools/fitness_fatigue.py
+++ tools/fitness_fatigue.py
@@ -15,7 +15,7 @@ def create_ff_df(df):
     ff_df = pd.DataFrame()
     start = df.index[0]
     start = datetime.fromtimestamp(start.timestamp())
-    end = datetime.today() + datetime.timedelta(days=7)
+    end = datetime.today() + datetime.timedelta(days=14)
     days = ((end-start).days)+1
     datelist = pd.date_range(start, periods=days).tolist()
     ff_df['date'] = datelist
@@ -42,11 +42,26 @@ def ff_model(ff_df,params):
     ff_df['fatigue'] = np.zeros(len(ff_df))
     ff_df['form'] = np.zeros(len(ff_df))
     
-    for i in range(len(b_trimps)):
+    for i in range(len(ff_df)):
+        if i == 0:
+            ff_df['fitness'][0] = 0 + (ff_df['b_trimp'][0] - 0)*(1-np.exp(-1/42))
+            ff_df['fatigue'][0] = 0 + (ff_df['b_trimp'][0] - 0)*(1-np.exp(-1/7))
+            ff_df['form'][0] = 0
+        else:
+            ff_df['fitness'][i] = ff_df['fitness'][i-1] + (ff_df['b_trimp'][i] - ff_df['fitness'][i-1])*(1-np.exp(-1/42))
+            ff_df['fatigue'][i] = ff_df['fatigue'][i-1] + (ff_df['b_trimp'][i] - ff_df['fatigue'][i-1])*(1-np.exp(-1/7))
+            ff_df['form'][i] = ff_df['fitness'][i-1] - ff_df['fatigue'][i-1]
+            
     
 def make_plot(ff_df):
     # Plot trimp over time
+    plt.figure()
     plt.plot(ff_df['date'], ff_df['b_trimp'],label='Banister TRIMP')
+    plt.plot(ff_df['date'], ff_df['fatigue'],label='Fatigue')
+    plt.plot(ff_df['date'], ff_df['fitness'],label='Fitness')
+    plt.plot(ff_df['date'], ff_df['form'],label='Form')
+    plt.hlines(0, ff_df['date'].loc[0], ff_df['date'].iloc[-1], linestyles='dashed')
+    plt.vlines(ff_df['date'].iloc[-14],-500,500, linestyles='dashed')
     plt.xticks(rotation=45)
     plt.subplots_adjust(bottom=0.2)
     plt.legend()
