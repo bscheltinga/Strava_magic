eecef7193bd125101054693dd7f5e9f0b98cc6ab
vuxxa <boukescheltinga@gmail.com>
2021-01-30 15:33:22 +0100

    Enhance dataframe. Now ready for fitness-fatigue implementation

--- tools/fitness_fatigue.py
+++ tools/fitness_fatigue.py
@@ -1,6 +1,7 @@
 # Implementation of fitness fatigue model
 from datetime import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 # assuming act. has HR_data
 # b_trimp: implementation of Banister' trimp (avg_hr*duration)
 
@@ -14,7 +15,7 @@ def create_ff_df(df):
     ff_df = pd.DataFrame()
     start = df.index[0]
     start = datetime.fromtimestamp(start.timestamp())
-    end = datetime.today()
+    end = datetime.today() + datetime.timedelta(days=7)
     days = ((end-start).days)+1
     datelist = pd.date_range(start, periods=days).tolist()
     ff_df['date'] = datelist
@@ -22,9 +23,30 @@ def create_ff_df(df):
     return ff_df
 
 def trimp_to_ff_df (b_trimps,ff_df):
+    # loop over all dates in df
+    # does not work for multiple activities on one day
+    ff_df['b_trimp'] = np.zeros(len(ff_df))
+    j = 0
+    for i in range(len(b_trimps)):
+        flag=True
+        while flag == True:
+            if b_trimps.index[i].date() == ff_df['date'][j].date():
+                ff_df['b_trimp'][j] = ff_df['b_trimp'][j] + b_trimps[i]
+                flag = False
+            else:
+                j += 1                
+    return ff_df
+
+def ff_model(ff_df,params):
+    ff_df['fitness'] = np.zeros(len(ff_df))
+    ff_df['fatigue'] = np.zeros(len(ff_df))
+    ff_df['form'] = np.zeros(len(ff_df))
     
-def make_plot(b_trimps)
+    for i in range(len(b_trimps)):
+    
+def make_plot(ff_df):
     # Plot trimp over time
-    plt.plot(b_trimps.index, b_trimps)
+    plt.plot(ff_df['date'], ff_df['b_trimp'],label='Banister TRIMP')
     plt.xticks(rotation=45)
     plt.subplots_adjust(bottom=0.2)
+    plt.legend()
