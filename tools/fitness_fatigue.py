47ae393c302a3dec3d9491405b5ddeb91b741878
vuxxa <boukescheltinga@gmail.com>
2021-02-01 18:07:20 +0100

    Add ACWR and calvert model.

--- tools/fitness_fatigue.py
+++ tools/fitness_fatigue.py
@@ -45,34 +45,73 @@ def trimp_to_ff_df (trimp_df,ff_df):
                 j += 1                
     return ff_df
 
-def ff_model(ff_df,params):
+def ff_model(ff_df,params, model='trainingspeaks'):
+    '''
+    Suported models: banister, trainingspeaks, calvert (not finished), ACWR
+    '''
     ff_df['fitness'] = np.zeros(len(ff_df))
     ff_df['fatigue'] = np.zeros(len(ff_df))
     ff_df['form'] = np.zeros(len(ff_df))
-    
-#    # Model equations according to trainingpeaks/elevate    
-#    for i in range(len(ff_df)):
-#        if i == 0:
-#            ff_df['fitness'][0] = 0 + (ff_df['b_trimp'][0] - 0)*(1-np.exp(-1/params[0]))
-#            ff_df['fatigue'][0] = 0 + (ff_df['b_trimp'][0] - 0)*(1-np.exp(-1/params[1]))
-#            ff_df['form'][0] = 0
-#        else:
-#            ff_df['fitness'][i] = ff_df['fitness'][i-1] + (ff_df['b_trimp'][i] - ff_df['fitness'][i-1])*(1-np.exp(-1/params[0]))
-#            ff_df['fatigue'][i] = ff_df['fatigue'][i-1] + (ff_df['b_trimp'][i] - ff_df['fatigue'][i-1])*(1-np.exp(-1/params[1]))
-#            ff_df['form'][i] = ff_df['fitness'][i-1] - ff_df['fatigue'][i-1]
+        
+    if model == 'trainingspeaks':
+        # Model equations according to trainingpeaks/elevate    
+        for i in range(len(ff_df)):
+            if i == 0:
+                ff_df['fitness'][0] = 0 + (ff_df['b_trimp'][0] - 0)*(1-np.exp(-1/params[0]))
+                ff_df['fatigue'][0] = 0 + (ff_df['b_trimp'][0] - 0)*(1-np.exp(-1/params[1]))
+                ff_df['form'][0] = 0
+            else:
+                ff_df['fitness'][i] = ff_df['fitness'][i-1] + (ff_df['b_trimp'][i] - ff_df['fitness'][i-1])*(1-np.exp(-1/params[0]))
+                ff_df['fatigue'][i] = ff_df['fatigue'][i-1] + (ff_df['b_trimp'][i] - ff_df['fatigue'][i-1])*(1-np.exp(-1/params[1]))
+                ff_df['form'][i] = ff_df['fitness'][i-1] - ff_df['fatigue'][i-1]
             
-    # Model equations according to equation 10/11 from
+    if model == 'banister':
+#     Model equations according to equation 10/11 from
     # B. S. Hemingway, L. Greig, and P. Swinton, “A NARRATIVE REVIEW OF MATHEMATICAL FITNESS-FATIGUE MODELLING FOR APPLICATIONS IN EXERCISE SCIENCE: MODEL DYNAMICS, METHODS, LIMITATIONS, AND FUTURE RECOMMENDATIONS A,” 2020, doi: 10.31236/osf.io/ap75j.
     # fitness/fatigue = Value yesterday*exp(param) + workload
-    for i in range(len(ff_df)):
-        if i == 0:
-            ff_df['fitness'][0] = 0 + (ff_df['b_trimp'][0])
-            ff_df['fatigue'][0] = 0 + (ff_df['b_trimp'][0])
-            ff_df['form'][0] = 0
-        else:
-            ff_df['fitness'][i] = ff_df['fitness'][i-1]*(np.exp(-1/params[0])) + ff_df['b_trimp'][i]
-            ff_df['fatigue'][i] = ff_df['fatigue'][i-1]*(np.exp(-1/params[1])) + ff_df['b_trimp'][i]
-            ff_df['form'][i] = ff_df['fitness'][i-1] - ff_df['fatigue'][i-1]
+        for i in range(len(ff_df)):
+            if i == 0:
+                ff_df['fitness'][0] = 0 + (ff_df['b_trimp'][0])
+                ff_df['fatigue'][0] = 0 + (ff_df['b_trimp'][0])
+                ff_df['form'][0] = 0
+            else:
+                ff_df['fitness'][i] = ff_df['fitness'][i-1]*(np.exp(-1/params[0])) + ff_df['b_trimp'][i]
+                ff_df['fatigue'][i] = ff_df['fatigue'][i-1]*(np.exp(-1/params[1])) + ff_df['b_trimp'][i]
+                ff_df['form'][i] = ff_df['fitness'][i-1] - ff_df['fatigue'][i-1]
+                
+    if model == 'calvert':
+#     Model equations according to equation 12 from
+    # B. S. Hemingway, L. Greig, and P. Swinton, “A NARRATIVE REVIEW OF MATHEMATICAL FITNESS-FATIGUE MODELLING FOR APPLICATIONS IN EXERCISE SCIENCE: MODEL DYNAMICS, METHODS, LIMITATIONS, AND FUTURE RECOMMENDATIONS A,” 2020, doi: 10.31236/osf.io/ap75j.
+    # fitness/fatigue = Value yesterday*exp(param) + workload (+ extra delay for fitness)
+        for i in range(len(ff_df)):
+            if i == 0:
+                ff_df['fitness'][0] = 0 + (ff_df['b_trimp'][0])
+                ff_df['fatigue'][0] = 0 + (ff_df['b_trimp'][0])
+                ff_df['form'][0] = 0
+            else:
+                ff_df['fitness'][i] = ff_df['fitness'][i-1]*(np.exp(-1/params[0]) - np.exp(-1/3)) + ff_df['b_trimp'][i]
+                ff_df['fatigue'][i] = ff_df['fatigue'][i-1]*(np.exp(-1/params[1])) + ff_df['b_trimp'][i]
+                ff_df['form'][i] = ff_df['fitness'][i-1] - ff_df['fatigue'][i-1]
+                
+    if model == 'ACWR':
+#     Model equations according to acute/chronic workload. Note that we not speak of fatigue and fintess but acute
+        # and chronic workload. The fitness: chronic, fatigue: acute and form (acute/chronic)
+        # it uses a rectangular window for the summation.
+        for i in range(len(ff_df)):
+            if i <= params[0] and i <= params[1]:
+                ff_df['fitness'][i] = sum(ff_df['b_trimp'][0:i])
+                ff_df['fatigue'][i] = sum(ff_df['b_trimp'][0:i])
+            elif i <= params[0] and i > params[1]:
+                ff_df['fitness'][i] = sum(ff_df['b_trimp'][0:i])
+                ff_df['fatigue'][i] = sum(ff_df['b_trimp'][i-params[1]:i])
+            else:
+                ff_df['fitness'][i] = sum(ff_df['b_trimp'][i-params[0]:i])
+                ff_df['fatigue'][i] = sum(ff_df['b_trimp'][i-params[1]:i])
+            
+            if i == 0:
+                ff_df['form'][i] = 0
+            else:
+                ff_df['form'][i] = ff_df['fatigue'][i-1]/ff_df['fitness'][i-1]
             
     return ff_df
     
@@ -89,11 +128,12 @@ def make_plot(ff_df):
     plt.xticks(rotation=45)
     plt.subplots_adjust(bottom=0.2)
     plt.legend()
-    
+ 
+model = 'ACWR'
 params = [42, 7] # [fitness, fatigue], [42, 7] as starting point
 trimp_df = b_trimp(df)
 ff_df = create_ff_df(df)
 ff_df = trimp_to_ff_df (trimp_df,ff_df)
-
-ff_df = ff_model(ff_df,params)
+ff_df = ff_model(ff_df, params, model)
 make_plot(ff_df)
+plt.title(model)
