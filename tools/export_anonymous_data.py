27cc1c92a74b801d81314beb05d1ffd4a8a78ac4
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2021-08-30 16:21:59 +0200

    Initialize a data export.py

---
+++ tools/export_anonymous_data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 30 16:16:57 2021
+
+This script will use the dataframe with all data and export each non-manual
+acitivity as .xlsx in the folder data/anonymous_data. The files contain
+all original data except for GPS-data. Sampling rate is the original rate.
+
+@author: bscheltinga
+"""
+
