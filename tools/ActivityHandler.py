9d1fda8612f89da67f788119c74f5086ac09ee58
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2024-06-11 12:47:12 +0200

    Change to new API limits

--- tools/ActivityHandler.py
+++ tools/ActivityHandler.py
@@ -165,7 +165,7 @@ class ActivityHandler(object):
             
             df_features = df_features.append(entry, ignore_index=True)
 
-            if self.__ApiLimitCounter > 595:
+            if self.__ApiLimitCounter > 195:
                 self.__waitAPIlimits()
                 
         df = pd.concat([df, df_features], axis=1)
